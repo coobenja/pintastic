352e75ccb3ae486724de889bffb06e9077cbf642
Ben Cooper <ben@wolverinetshirtcompany.com>
2015-05-14 14:44:14 -0400

    Devise working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Put in actual host url
+  config.action_mailer.default_url_options = { :host => 'https://tut-pintastic.herokuapp.com/#' }
+
 end

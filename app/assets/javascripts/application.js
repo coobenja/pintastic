7da43a9038be48c97663e521a59649724f568fb4
Ben Cooper <ben@wolverinetshirtcompany.com>
2015-05-15 13:26:55 -0400

    Added jQuery masonry

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //= require jquery
 //= require jquery_ujs
+//= require jquery.turbolinks
 //= require bootstrap
 //= require turbolinks
+//= require masonry/jquery.masonry
 //= require_tree .
